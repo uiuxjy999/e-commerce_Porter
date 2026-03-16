388228fa532f5460afdae1bbb31fdf509eec3f90
kkimna0101 <kkimna0101@gmail.com>
2026-03-16 15:05:38 +0900

    Fix SCSS variable resolution and image path for NotFound

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './NotFound.scss';
-import errorImg from '../img/porter/404page.png';
+const errorImg = '/images/404_error.png';
 
 const ArrowRightIcon = () => (
   <svg width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
